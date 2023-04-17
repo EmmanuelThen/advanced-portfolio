d26333f1b254eda64490b2fe84d3021545f0c2f4
Emmanuel Then <emmanuel.then@outlook.com>
2023-04-17 00:15:58 -0400

    start of new advanced portfolio build

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+
+    <div>
+      <head>
+        Emmanuel's Portfolio
+      </head>
+
+      {/*Header*/}
+      
+
+
+      {/*Hero*/}
+
+
+      {/*About*/}
+
+
+      {/*Experiences*/}
+
+      
+      {/*Skills*/}
+
+
+      {/*Projects*/}
+
+
+      {/*Contact Me*/}
+      
+    </div>
+
+  )
+}
